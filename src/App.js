8be28c59f1722872568a9edd1a0571c271452706
Shivam Rai <SHRunited0702@gmail.com>
2024-08-10 10:33:50 +0530

    first comiit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+// import Navbar from './components/navbars/navbar1';
+import Form from './components/forms/from1';
+// import Loader from './components/Loaders/loader1';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <Switch>
+          <Route path="/navbar" component={Navbar} />
+          <Route path="/form" component={Form} />
+          <Route path="/loader" component={Loader} />
+          {/* Add more routes as necessary */}
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
